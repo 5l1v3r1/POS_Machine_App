4441ce3f0767b593dbf9e9c2d8e0cd2f1306cf54
unknown <kathor888@kert.or.kr>
2019-03-27 16:23:05 +0900

    refactoring file

--- SJ/RestaurantMain.java
+++
@@ -1,9 +0,0 @@
-public class RestaurantMain {
-//수제 버거
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		StartRestaurant gui= new StartRestaurant();
-		gui.setVisible(true);
-	}
-
-}
